c8f662511a7723145e03f4efecc23554219b09c4
Hicham Es-Saquy <hes-saqu@c4r9p3.1337.ma>
2025-10-27 22:25:06 +0100

    inception

--- Makefile
+++ Makefile
@@ -1,15 +1,12 @@
 create:
 	@mkdir ~/data ~/data/wordpress_database/ ~/data/wordpress_files/ 
-
 build:
 	sudo docker compose -f ./srcs/docker-compose.yml build
 run:
 	sudo docker compose -f ./srcs/docker-compose.yml up
-
 clean:
 	sudo docker compose -f ./srcs/docker-compose.yml down
-	sudo docker image rm wordpress:1337 nginx:1337 mariadb:1337 
+	sudo docker image rm wordpress:1337 nginx:1337 mariadb:1337
 vclear:
 	sudo rm -rf ~/data/wordpress_database/* ~/data/wordpress_files/*
-
 rebuild: clean build
--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -3,32 +3,32 @@ mkdir -p /var/www/html
 
 cd /var/www/html
 
-wget -O wp-cli.phar https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+if [-f ./wp-config.php ]
+	then echo "wordpress already installed"
+else
+	wget -O wp-cli.phar https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
-chmod +x wp-cli.phar
-sudo mv wp-cli.phar /usr/local/bin/wp
+	chmod +x wp-cli.phar
+	sudo mv wp-cli.phar /usr/local/bin/wp
 
-wp core download --allow-root
+	wp core download --allow-root
 
-cp wp-config-sample.php wp-config.php
+	cp wp-config-sample.php wp-config.php
 
-sudo sed -i "s/database_name_here/$DBNAME/g" wp-config.php 
-sudo sed -i "s/username_here/$DBUSER/g" wp-config.php 
-sudo sed -i "s/password_here/$DBPASS/g" wp-config.php 
-sudo sed -i "s/define( 'DB_HOST', 'localhost' );/define( 'DB_HOST', 'mariadb:3306' );/g" wp-config.php 
-wp config set WP_REDIS_HOST "redis" --allow-root
-wp config set WP_REDIS_PORT "6379" --allow-root
-sleep 5
+	sudo sed -i "s/database_name_here/$DBNAME/g" wp-config.php 
+	sudo sed -i "s/username_here/$DBUSER/g" wp-config.php 
+	sudo sed -i "s/password_here/$DBPASS/g" wp-config.php 
+	sudo sed -i "s/define( 'DB_HOST', 'localhost' );/define( 'DB_HOST', 'mariadb:3306' );/g" wp-config.php 
+	sleep 5
 
-wp core install --skip-email --url=$DOMAIN_NAME --title='Inception' --admin_user=$ADMIN_USER  --admin_email=$ADMIN_EMAIL --admin_password=$ADMIN_PASS --allow-root
-
-wp plugin install redis-cache --activate --allow-root
-wp redis enable --force --allow-root
-
-sudo chown -R www-data:www-data /var/www/html
-sudo chmod -R 755 /var/www/html
-
-sudo sed -i 's/listen = \/run\/php\/php8.2-fpm.sock/listen = 9000/g' /etc/php/8.2/fpm/pool.d/www.conf
+	wp core install --skip-email --url=$DOMAIN_NAME --title='Inception' --admin_user=$ADMIN_USER  --admin_email=$ADMIN_EMAIL --admin_password=$ADMIN_PASS --allow-root
 
 
-php-fpm8.2 -F
+	sudo chown -R www-data:www-data /var/www/html
+	sudo chmod -R 755 /var/www/html
+
+	sudo sed -i 's/listen = \/run\/php\/php8.2-fpm.sock/listen = 9000/g' /etc/php/8.2/fpm/pool.d/www.conf
+	cp wp-config-sample.php wp-config.php
+fi
+
+exec "@a"
